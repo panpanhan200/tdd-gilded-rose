78b96b896066c14de49493da8188fb8b38da4264
Panpan Han <hanpanpan200@gmail.com>
2020-03-24 00:09:41 +0800

    Refactor property name

--- src/main/java/cn/xpbootcamp/gilded_rose/Item.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Item.java
@@ -2,12 +2,12 @@ package cn.xpbootcamp.gilded_rose;
 
 public class Item {
 
-    public final String productName;
+    public final String name;
     public final int sellIn;
     public int quality;
 
-    public Item(String productName, int sellIn, int quality) {
-        this.productName = productName;
+    public Item(String name, int sellIn, int quality) {
+        this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
